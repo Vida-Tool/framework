5b08847676606a8a57274466d610a0b3cd599557
nuribatuhansari <nuribatuhansari@hotmail.com>
2023-11-26 18:14:27 +0300

    Update TextureLoader.cs

--- Editor/Runtime/Helpers/TextureLoader.cs
+++ Editor/Runtime/Helpers/TextureLoader.cs
@@ -5,23 +5,20 @@ namespace Vida.Editor
 {
     public class TextureLoader
     {
+        
         public static Texture2D GetTexture(string path)
         {
             string normalPath = "Assets/framework/Editor/Resources/Icons/" + path;
-            string packagePath = "Packages/framework/Editor/Resources/Icons/" + path;
-            string packageCachePath = "Library/PackageCache/framework/Editor/Resources/Icons/" + path;
+            string packagePath = "Packages/com.vida.framework/Editor/Resources/Icons/" + path;
 
             var item = AssetDatabase.LoadAssetAtPath<Texture2D>(normalPath);
 
             if (!item)
             {
-                item = AssetDatabase.LoadAssetAtPath<Texture2D>(packageCachePath);
-            }
-            
-            if (!item)
-            {
                 item = AssetDatabase.LoadAssetAtPath<Texture2D>(packagePath);
             }
+            
+ 
             
             
 
