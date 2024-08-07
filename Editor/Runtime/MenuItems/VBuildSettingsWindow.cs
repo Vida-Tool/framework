a2cf024ad2c2d1ca55883a92298007fdfecff00c
Batuhan <batuhan@vida.games>
2024-08-07 15:29:27 +0300

    VBuildSettings Update

--- Editor/Runtime/MenuItems/VBuildSettingsWindow.cs
+++ Editor/Runtime/MenuItems/VBuildSettingsWindow.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,20 +10,41 @@ namespace Vida.Framework
         private int bundleCode = 1;
         private void OnDestroy()
         {
-            EditorApplication.projectChanged -= OnProjectChanged;
+            EditorApplication.update -= OnProjectChanged;
+        }
+
+        public void BuildSettingsClosed()
+        {
+            Close();
+        }
+
+        public void BuildSettingsUpdate()
+        {
+            bundleCode = PlayerSettings.Android.bundleVersionCode;
+            version = PlayerSettings.bundleVersion;
+            
+            bool dock = EditorPrefs.GetBool("VidaDocking", true);
+
+            if (dock)
+            {
+                DockNextTo(this, GetActiveWindow("BuildPlayerWindow"));
+            }
         }
 
         [MenuItem("Vida/VBuild Settings")]
         public static void ShowBuildSettingsWindow()
         {
+            EditorApplication.update += OnProjectChanged;
+
             var w = GetWindow<VBuildSettingsWindow>("VBuild Settings");
             w.bundleCode = PlayerSettings.Android.bundleVersionCode;
             w.version = PlayerSettings.bundleVersion;
             w.maxSize = new Vector2(800, 400);
-            EditorApplication.projectChanged += OnProjectChanged;
             
-            float xCenter = Screen.currentResolution.width / 2 - w.position.width;
-            float yCenter = Screen.currentResolution.height / 2 - w.position.height;
+
+            
+            float xCenter = Screen.currentResolution.width / 2f - w.position.width;
+            float yCenter = Screen.currentResolution.height / 2f - w.position.height;
             // if position is outside of the screen, reset it
             if (w.position.x > Screen.currentResolution.width || w.position.y > Screen.currentResolution.height)
             {
@@ -33,18 +55,34 @@ namespace Vida.Framework
                 w.position = new Rect(xCenter, yCenter, 800, 400);
             }
 
-            var build = GetBuildPlayerWindow();
+            var build = GetActiveWindow("BuildPlayerWindow");
             if (build)
             {
                 DockNextTo(w, build);
             }
         }
         
+        
+        
         private static void OnProjectChanged()
         {
-            var w = GetWindow<VBuildSettingsWindow>("VBuild Settings");
-            w.bundleCode = PlayerSettings.Android.bundleVersionCode;
-            w.version = PlayerSettings.bundleVersion;
+            var w = GetActiveWindow("VBuildSettingsWindow");
+            if (w)
+            {
+                var b = w as VBuildSettingsWindow;
+                b.bundleCode = PlayerSettings.Android.bundleVersionCode;
+                b.version = PlayerSettings.bundleVersion;
+                
+                bool dock = EditorPrefs.GetBool("VidaDocking", true);
+
+                // only if repaint
+                
+                
+                if (dock)
+                {
+                    DockNextTo(b, GetActiveWindow("BuildPlayerWindow"));
+                }
+            }
         }
 
         void OnGUI()
@@ -68,10 +106,6 @@ namespace Vida.Framework
 
             EditorPrefs.SetBool
                 ("OpenVBuildSettings",GUILayout.Toggle(EditorPrefs.GetBool("OpenVBuildSettings", true), "Show VBuild"));
-            EditorPrefs.SetBool
-                ("OpenProjectSettings",GUILayout.Toggle(EditorPrefs.GetBool("OpenProjectSettings", false), "Show Settings"));
-
-            
 
             // Apply button
             if (GUILayout.Button("Apply"))
@@ -98,7 +132,7 @@ namespace Vida.Framework
                 dock = !dock;
                 EditorPrefs.SetBool("VidaDocking", dock);
                 
-                var build = GetBuildPlayerWindow();
+                var build = GetActiveWindow("BuildPlayerWindow");
                 if (dock && build)
                 {
                     DockNextTo(this, build);
@@ -108,37 +142,52 @@ namespace Vida.Framework
 
             if (dock)
             {
-                DockNextTo(this, GetBuildPlayerWindow());
+                DockNextTo(this, GetActiveWindow("BuildPlayerWindow"));
             }
             
             GUILayout.EndVertical();
         }
         
-        private static EditorWindow GetBuildPlayerWindow()
-        {
-            // Tüm açık EditorWindow'ları al
-            var windows = Resources.FindObjectsOfTypeAll<BuildPlayerWindow>();
 
-            // Her bir pencereyi kontrol et
+        private static EditorWindow GetActiveWindow(string windowName)
+        {
+            var windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+
             foreach (var window in windows)
             {
-                if (window.GetType().Name == "BuildPlayerWindow")
+                if (window.GetType().Name == windowName)
                 {
                     return window;
                 }
             }
 
-            // Eğer bulamazsak null döneriz
             return null;
         }
-        
+
+        private static List<Vector3> positions = new List<Vector3>();
         private static void DockNextTo(EditorWindow main, EditorWindow docked)
         {
             if(docked == null) return; 
             // Get the position of the Build Settings window
             Rect dockedPos = docked.position;
+            positions.Add(new Vector3(dockedPos.x, dockedPos.y, 0));
+            if(positions.Count >= 5) positions.RemoveAt(0);
+            
+            // if all positions not same return
+            if (positions.Count > 1)
+            {
+                for (int i = 1; i < positions.Count; i++)
+                {
+                    if (positions[i] != positions[i - 1])
+                    {
+                        return;
+                    }
+                }
+            }
+            
             // Set the position of the VBuild Settings window next to the Build Settings window
             Rect mainTargetPos = new Rect(dockedPos.x + dockedPos.width, dockedPos.y, main.position.width, main.position.height);
+            
             main.position = mainTargetPos;
         }
     }
